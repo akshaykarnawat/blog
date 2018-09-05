66b1328bfa0d93d995155882d4cb2215e5d0d83b
akshaykarnawat <akshaykarnawat@riverbed.local>
2018-09-04 23:29:32 -0400

    Updates

---
+++ static/about-5bd474683e2430255736a31f501927ba.js
@@ -0,0 +1,28 @@
+import React from "react"
+import Container from "../components/container"
+
+export default () => {
+    return (
+        <Container>
+            <p>A simple blog built using gatsbyjs.</p>
+            <br/><br/>
+            {/* add placed below */}
+            <div style={{height: '250px', width: '500px'}}
+                dangerouslySetInnerHTML={{
+                    __html: `   
+                            <script type="text/javascript">
+                                ( function() {
+                                if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
+                                var unit = {"calltype":"async[2]","publisher":"akarnawat","width":550,"height":250,"sid":"Chitika Default"};
+                                var placement_id = window.CHITIKA.units.length;
+                                window.CHITIKA.units.push(unit);
+                                document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
+                                }());
+                            </script>
+                            <script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>
+                            `,
+                }}
+            />
+        </Container>
+    )
+}
